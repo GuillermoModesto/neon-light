9d6c2f45dab03c69c6a893ab424bf7ff45501ec0
GuillermoModesto <152183708+GuillermoModesto@users.noreply.github.com>
2024-12-16 14:45:54 +0100

    intento de crear edificios

--- code/main.js
+++ code/main.js
@@ -1,18 +1,40 @@
-window.onload = function() {
+const building_appear = 2;
+const time_eddie_multiplier = 0.25;
+const amount_eddie_increase = 1;
+let eddies = 0;
+
+window.onload = function()
+{
     let eddies_tag = document.getElementById("eddies");
-    let transmute_button = document.getElementById("transmute");
-    transmute_button.addEventListener("click", function() {
-        generate_eddie(eddies_tag, transmute_button);
+    document.getElementById("transmute").addEventListener("click", function()
+    {
+        eddies = update_eddies(eddies_tag, this);
+        console.log(eddies)
+        
     });
 }
 
-function generate_eddie(eddies_tag, transmute_button) {
+// Generate 1 eddie
+function update_eddies(eddies_tag, transmute_button)
+{
     transmute_button.disabled = true;
-    let _eddies = Number(eddies_tag.innerText);
-    let time = _eddies * 0.25 * 1000;
+    let time = eddies * time_eddie_multiplier * 1000;
     setTimeout(function() {
-        _eddies += 1;
-        eddies_tag.innerText = _eddies;
+        eddies += amount_eddie_increase;
+        eddies_tag.innerText = eddies;
         transmute_button.disabled = false;
     }, time);
+    return eddies;
+}
+
+function create_buildings_container()
+{
+    let select = document.createElement("select");
+    select.setAttribute("name", "edificios");
+    select.setAttribute("id", "edificios");
+    let option = document.createElement("option");
+    option.setAttribute("value", "container");
+    option.appendChild(document.createTextNode("Container"));
+    select.append(option);
+    document.forms[0].appendChild(select);
 }
