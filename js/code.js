e2fb1661457719230060bb511c7fbc8e94c15a84
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-12-21 13:44:23 +0100

    añadir primera opcion al panel y gestionar su uso

--- js/code.js
+++ js/code.js
@@ -3,10 +3,10 @@ const building = {
     warehouse: { cost: 2, built: false }
 };
 
-/* Usado en getElapsedTime */
+/* for getElapsedTime */
 let start = performance.now();
 
-/* Usado en update */
+/* for update */
 let stamp = performance.now();
 let total_time = 0;
 
@@ -20,7 +20,7 @@ window.onload = function () {
 
 function generate_eddie(button) {
     button.disabled = true;
-    button.style.filter = "invert(41%) sepia(36%) saturate(19%) hue-rotate(314deg) brightness(95%) contrast(98%)";
+    visual_disable(button);
     let eddie_timer = 250 * resource.eddie;
     setTimeout(function () {
         resource.eddie++;
@@ -33,10 +33,9 @@ function generate_eddie(button) {
 
 function check_building_btn() {
     if (document.getElementById("building_btn") == null && resource.eddie >= 2) {
-
         create_building_panel();
-        create_exit_panel_btn(document.getElementById("buildings_panel"));
         add_option_to_panel("warehouse");
+        create_exit_panel_btn(document.getElementById("buildings_panel"));
         create_building_btn();
     }
 }
@@ -45,7 +44,6 @@ function create_building_panel() {
     let building_panel = document.createElement("div");
     building_panel.setAttribute("class", "cyber_panel--hidden");
     building_panel.setAttribute("id", "buildings_panel");
-    // building_panel.appendChild(document.createTextNode("me quiero de morí jajaj"));
     document.getElementsByClassName("game-area")[0].appendChild(building_panel);
 }
 
@@ -55,10 +53,16 @@ function create_exit_panel_btn(panel) {
     exit_building.setAttribute("id", "exit_building");
     exit_building.appendChild(document.createTextNode("X"));
     panel.appendChild(exit_building);
+
     exit_building.addEventListener("click", function () {
         panel.setAttribute("class", "cyber_panel--hidden cyber_panel--hidden--animation");
         document.getElementById("building_btn").style.zIndex = 3;
         document.getElementById("overlay").style.zIndex = -1;
+        for (const child of panel.children) {
+            child.hidden = true;
+            child.disabled = true;
+            child.style.display = "none";
+        }
     });
 }
 
@@ -68,6 +72,15 @@ function add_option_to_panel(option) {
     panel_option.setAttribute("class", "panel_option");
     panel_option.appendChild(document.createTextNode(option));
     document.getElementById("buildings_panel").appendChild(panel_option);
+    panel_option.addEventListener("click", function() {
+        let selectedBuilding = building[option];
+        if (resource.eddie >= selectedBuilding.cost && !selectedBuilding.built) {
+            resource.eddie -= selectedBuilding.cost;
+            selectedBuilding.built = true;
+            visual_disable(panel_option);
+            updateUI();
+        }
+    });
 }
 
 function create_building_btn() {
@@ -76,11 +89,22 @@ function create_building_btn() {
     building_btn.setAttribute("id", "building_btn");
     building_btn.setAttribute("class", "cyber_btn");
     document.getElementsByClassName("game-area")[0].appendChild(building_btn);
+
     building_btn.addEventListener("click", function () {
+        let panel = document.getElementById("buildings_panel");
         document.getElementById("buildings_panel").setAttribute("class", "cyber_panel");
         building_btn.style.zIndex = 0;
         document.getElementById("overlay").style.zIndex = 0;
+        for (const child of panel.children) {
+            child.hidden = false;
+            child.disabled = false;
+            child.style.display = "";
+        }
     });
+}
+
+function visual_disable(button) {
+    button.style.filter = "invert(41%) sepia(36%) saturate(19%) hue-rotate(314deg) brightness(95%) contrast(98%)";
 }
 
 /* Update all editable HTML tags. All IDs should match variable name to work on loop */
